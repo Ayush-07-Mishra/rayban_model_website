b387fbc52bc5fba3c80453d188f41213ac343468
Ayush-07-Mishra <ayushmishra312004@gmail.com>
2024-04-08 15:06:34 +0530

    frontend

---
+++ js/script.js
@@ -0,0 +1,40 @@
+//Shery.imageEffect("#back",  {style: 5, debug:true, gooey:true});
+
+// run the above line first whithiut coding the below lines the panel gets opean just give the values and then copy to keyboard then paste it 
+// iin place of the debug : true 
+
+Shery.imageEffect("#back",  {style: 5, config:{"a":{"value":1.6,"range":[0,30]},
+"b":{"value":-0.98,"range":[-1,1]},
+"zindex":{"value":-9996999,"range":[-9999999,9999999]},
+"aspect":{"value":1.7952314165497896},
+"ignoreShapeAspect":{"value":true},
+"shapePosition":{"value":{"x":0,"y":0}},
+"shapeScale":{"value":{"x":0.5,"y":0.5}},
+"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},
+"shapeRadius":{"value":0,"range":[0,2]},
+"currentScroll":{"value":0},
+"scrollLerp":{"value":0.07},
+"gooey":{"value":true},
+"infiniteGooey":{"value":true},
+"growSize":{"value":4,"range":[1,15]},
+"durationOut":{"value":1,"range":[0.1,5]},
+"durationIn":{"value":1,"range":[0.1,5]},
+"displaceAmount":{"value":0.5},
+"masker":{"value":false},
+"maskVal":{"value":1.03,"range":[1,5]},
+"scrollType":{"value":0},
+"geoVertex":{"range":[1,64],"value":1},
+"noEffectGooey":{"value":false},
+"onMouse":{"value":1},
+"noise_speed":{"value":0.61,"range":[0,10]},
+"metaball":{"value":0.2,"range":[0,2],"_gsap":{"id":3}},
+"discard_threshold":{"value":0.5,"range":[0,1]},
+"antialias_threshold":{"value":0.05,"range":[0,0.1]},
+"noise_height":{"value":0.49,"range":[0,2]},
+"noise_scale":{"value":9.92,"range":[0,100]}} ,
+ gooey: true});
+
+// document.querySelector("#main").addEventListener("click", function() {
+//     alert("The main element was clicked!");
+// });
+
